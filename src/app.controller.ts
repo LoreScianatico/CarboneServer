6be6783c6d9aab74a4ad419c66d564db42191050
e514001 <lorenzo.scianatico@sbb.ch>
2022-02-05 11:05:25 +0100

    Printing template, no conversion

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('report')
+  getReport(@Res() response: Response): void { //warning! Express.js specific!
+    this.appService.printReport(response);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+//import carbone from 'carbone'; does not work, returns undefined
+import { Response } from 'express';
+
+const carbone = require('carbone');
 
 @Injectable()
 export class AppService {
+
+    //constructor(private readonly carbone) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  printReport(response: Response): void {
+    var data = {
+      firstname : 'John',
+      lastname : 'Doe'
+    };
+    carbone.render('./node_modules/carbone/examples/simple.odt', data, (err: any, result: any) => {
+        if (err) {
+          return console.log(err);
+        }
+        // write the result
+        response.header('Content-Type', 'application/vnd.oasis.opendocument.text');
+        response.header('Content-disposition', `attachment; filename=simple.odt`);
+        response.write(result, 'binary');
+        response.end();
+      });
   }
 }
